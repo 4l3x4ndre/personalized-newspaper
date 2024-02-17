8ef47daf279766d94f1c9bae8c3618a49b5a244e
4l3x4ndre <alexandre@amrani.fr>
2024-02-17 11:16:35 +0100

    install files - sudo apt

--- install_app.sh
+++ install_app.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Navigate to your project directory (adjust the path as necessary)
-#cd /path/to/your-flask-app
-
 # Step 1: Update and Install Python3 & pip
-sudo pt-get update
+sudo apt-get update
 sudo apt-get install -y python3 python3-pip python3-venv
 
 # Step 2: Setup Python Virtual Environment
@@ -14,21 +11,11 @@ python3 -m venv venv
 source venv/bin/activate
 
 # Step 4: Install Requirements
-pip install -r utils/requirements.txt
+pip3 install -r utils/requirements.txt
 
 python -m pip install -U --pre 'urllib3>=2.0.0a1'
 
-# Optional: Setup Gunicorn to serve the app and Nginx as a reverse proxy
-#pip install gunicorn
-# For Nginx setup, you would need to configure it manually or add additional commands here
-
 echo "Installation is complete. Your Flask app is ready to be served!"
 
-# Additional commands to start your Flask application can go here
-# For example, to start a development server:
-# flask run --host=0.0.0.0
+# Step 5: Run the Flask App
 flask --app app.py  run 
-
-# Or to start with Gunicorn:
-# gunicorn -w 4 -b 0.0.0.0:8000 run:app
-
